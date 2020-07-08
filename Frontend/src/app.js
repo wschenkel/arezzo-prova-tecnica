ed50a443760d2646f924964fa27d84319d141283
Willian Schenkel <hischenkel@gmail.com>
2020-07-07 21:16:55 -0300

    added list products

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,22 +1,9 @@
 import Header from '../src/materials/components/header/header.js';
+import Main from '../src/materials/components/header/Main.js';
 export default class App {
 	constructor() {
 		new Header();
-
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://localhost:3004/api/products.json', false);
-
-		try {
-			xhr.send();
-			if (xhr.status != 200) {
-				console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-			} else {
-				// console.log(xhr.response);
-			}
-		} catch (err) {
-			console.log(err);
-			alert('Falha ao carregar os dados.');
-		}
+		new Main();
 	}
 }
 
