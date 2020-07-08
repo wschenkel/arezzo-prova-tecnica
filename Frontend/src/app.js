8fc6923874fbaf031009bc38bd8c47fe31fdeacc
Willian Schenkel <hischenkel@gmail.com>
2020-07-07 23:06:38 -0300

    Added list products

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,5 +1,5 @@
 import Header from '../src/materials/components/header/header.js';
-import Main from '../src/materials/components/header/Main.js';
+import Main from '../src/materials/components/main/Main.js';
 export default class App {
 	constructor() {
 		new Header();
