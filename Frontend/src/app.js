44a86961964598938c2d3aad55420c2965d106fe
Willian Schenkel <hischenkel@gmail.com>
2020-07-09 01:13:47 -0300

    added change page on click arrow

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -4,29 +4,31 @@ export default class App {
 		new Header();
 
 		var responseProducts = this.getProducts();
-		
 		const arrProducts = {
 			products: responseProducts,
 			total: responseProducts.length,
 			pages: Math.ceil(responseProducts.length / 24),
-			per_page: 24,
-			current_page: 1,
-			// next_page: 2,
+			perPage: 24,
+			currentPage: 1,
+			previousPage: 1,
+			nextPage: 2,
 			initialSlice: 0,
 			finalSlice: 24
 		}
 		
-		// Initialize ordenaiton by price.
-		this.createPagination(arrProducts);
+		// Initialize ordenation by price.
 		this.orderProducts('highest', arrProducts);
-
+		this.createPagination(arrProducts);
+		
 		// Select
 		const selOrder = document.getElementsByClassName('selOrderProducts')[0];
 		selOrder.addEventListener('change', () => {
-			arrProducts.current_page = 1;
+			arrProducts.currentPage = 1;
+			arrProducts.nextPage = 2;
+			arrProducts.previousPage = 1;
 			arrProducts.initialSlice = 0;
 			arrProducts.finalSlice = 24;
-			this.setAnchorActive(1);
+			this.setAnchorActive(arrProducts);
 			this.orderProducts(selOrder.value, arrProducts);
 		});
 
@@ -34,36 +36,74 @@ export default class App {
 		document.querySelectorAll('.page-anchor').forEach(page => {
 			page.addEventListener('click', event => {
 				let page = event.target.getAttribute('data-page');
-				this.setAnchorActive(page);
 				this.changePage(page, arrProducts);
+				this.setAnchorActive(arrProducts);
 			});
+		});
+
+		const btnPrevious = document.getElementsByClassName('btn-previous-page')[0];
+		btnPrevious.addEventListener('click', () => {
+			this.changePage(arrProducts.previousPage, arrProducts);
+			this.setAnchorActive(arrProducts);
+		});
+
+		const btnNext = document.getElementsByClassName('btn-next-page')[0];
+		btnNext.addEventListener('click', () => {
+			this.changePage(arrProducts.nextPage, arrProducts);
+			this.setAnchorActive(arrProducts);
 		});
 	}
 
-	setAnchorActive(page) {
+	setAnchorActive(arrProducts) {
+
 		document.querySelectorAll('.page-anchor').forEach(anchor => {
 			anchor.classList.remove('active');
-			if (anchor.getAttribute('data-page') == page) {
+			if (anchor.getAttribute('data-page') == arrProducts.currentPage) {
 				anchor.classList.add('active');
 			}
 		});
+
+		if (arrProducts.pages === arrProducts.currentPage) {
+			document.getElementsByClassName('btn-next-page')[0].classList.add('blocked');
+		} else {
+			document.getElementsByClassName('btn-next-page')[0].classList.remove('blocked');
+		}
+
+		if (arrProducts.currentPage === arrProducts.previousPage) {
+			document.getElementsByClassName('btn-previous-page')[0].classList.add('blocked');
+		} else {
+			document.getElementsByClassName('btn-previous-page')[0].classList.remove('blocked');
+		}	
 	}
 
 	changePage(page, arrProducts) {
-		arrProducts.current_page = page;
+
+		arrProducts.currentPage = parseInt(page, 10);
 		if (page === 1) {
 			arrProducts.initialSlice = 0;
-			arrProducts.finalSlice = arrProducts.per_page;
+			arrProducts.finalSlice = arrProducts.perPage;
+			arrProducts.nextPage = page + 1;
+			arrProducts.previousPage = page;
 		} else {
-			arrProducts.initialSlice = (page * arrProducts.per_page) - arrProducts.per_page;
+			arrProducts.initialSlice = (page * arrProducts.perPage) - arrProducts.perPage;
 			
-			let finalSlice = page * arrProducts.per_page;
+			let finalSlice = page * arrProducts.perPage;
+			arrProducts.finalSlice = finalSlice;
 			if (finalSlice > arrProducts.total) {
 				arrProducts.finalSlice = arrProducts.total;
-			} else {
-				arrProducts.finalSlice = finalSlice;
 			}	
+
+			arrProducts.previousPage = parseInt(page) - 1;
+			if ((parseInt((page)) - 1) === 0) {
+				arrProducts.previousPage = 1;
+			}
+
+			arrProducts.nextPage = parseInt(page) + 1;
+			if ((parseInt((page)) + 1) > arrProducts.pages) {
+				arrProducts.nextPage = parseInt(arrProducts.pages, 10);
+			}
 		}
+
 		this.listBuilder(arrProducts);
 	}
 
@@ -74,7 +114,7 @@ export default class App {
 		for (var i = 1; i <= arrProducts.pages; i++) {
 			itemsBuilder += `
 			<li>
-            	<a data-page="${i}" class="page-anchor ${ arrProducts.current_page == i ? 'active' : ''}" href="javascript:void(0);">${i}</a>
+            	<a data-page="${i}" class="page-anchor ${ arrProducts.currentPage == i ? 'active' : ''}" href="javascript:void(0);">${i}</a>
         	</li>
 			`;
 		}
